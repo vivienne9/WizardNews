8e1d0d5556d7d8c8b4d0fc5200718590adf0cb69
Vivienne Chawande <113947648+vivienne9@users.noreply.github.com>
2023-01-05 20:44:43 -0600

    updated app

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
 const express = require("express");
-const morgan = require(`morgan`);
-const postBank = require(`./postBank`);
-
-
+const morgan = require("morgan");
+const postBank = require("./postBank1")
 const app = express();
 
-app.use(morgan(`dev`));
+app.use(morgan('dev'))
 
+app.get("/", (req, res) => {
 
-app.get(`/`, (req, res) => {
-  
   const posts = postBank.list();
 
-  console.log(`/ I want this route `);
-  
-  const html = 
-  `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header>Wizard News</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            ${post.title}
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`
+  const html = `<!DOCTYPE html>
+    <html>
+      <head>
+       <title>test</title>
+       </head>
+       <body>
+        <ul>
+        </ul>
+        </body>
+        </html>
 
-res.send(html);
-});
+  
+  
+  `
+
+
 
 const PORT = 1337;
 
