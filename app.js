b2d48bd44171951395d54030a8b746d243bec8d0
Silver Ice <jice391@gmail.com>
2023-01-07 18:17:30 -0500

    Updated App

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.get("/posts/:id", (req, res) => {
   };
 })
 
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
